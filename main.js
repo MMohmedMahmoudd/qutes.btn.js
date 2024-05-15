f6376f7c27d9926e0773f3611b01508bf3bd5f7a
MMohmedMahmoudd <162662856+MMohmedMahmoudd@users.noreply.github.com>
2024-05-15 08:03:34 +0300

    third commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.getElementById('quoteBtn').addEventListener('click', function() {
     const quotes = [
         '"Life is what happens when youre busy making other plans"\n --John Lennon',
         '"Get busy living or get busy dying."\n Stephen King',
-        '"You only live once, but if you do it right, once is enough." \n --Mae West',
+        '"You only live once, but if you do it --right, once is enough." \n --Mae West',
         '"Many of life’s failures are people who did not realize how close they were to success when they gave up." \n --Thomas A. Edison',
         '"If you want to live a happy life, tie it to a goal, not to people or things." \n --Albert Einstein'
     ];
