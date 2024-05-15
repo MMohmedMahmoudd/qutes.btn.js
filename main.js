dfaa60b26d4127db227b5d95466db8cad25ffff0
MMohmedMahmoudd <162662856+MMohmedMahmoudd@users.noreply.github.com>
2024-05-15 07:57:55 +0300

    third commit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 document.getElementById('quoteBtn').addEventListener('click', function() {
     const quotes = [
-        "Life is what happens when you're busy making other plans\n John Lennon",
-        "Get busy living or get busy dying. \n Stephen King",
-        "You only live once, but if you do it right, once is enough. \n Mae West",
-        "Many of life’s failures are people who did not realize how close they were to success when they gave up. \n Thomas A. Edison",
-        "If you want to live a happy life, tie it to a goal, not to people or things. \n Albert Einstein"
+        '"Life is what happens when youre busy making other plans"\n --John Lennon',
+        '"Get busy living or get busy dying."\n Stephen King',
+        '"You only live once, but if you do it right, once is enough." \n --Mae West',
+        '"Many of life’s failures are people who did not realize how close they were to success when they gave up." \n --Thomas A. Edison',
+        '"If you want to live a happy life, tie it to a goal, not to people or things." \n --Albert Einstein'
     ];
     const randomIndex = Math.floor(Math.random() * quotes.length);
     document.getElementById('quote').innerText = quotes[randomIndex];
